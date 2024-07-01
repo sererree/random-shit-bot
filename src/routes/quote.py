26cef3a3e2cfd215a2984ca1c28f45d956ae4eb8
renya <159530999+sererree@users.noreply.github.com>
2024-07-01 11:39:19 +0300

    shit

--- src/routes/quote.py
+++ src/routes/quote.py
@@ -2,24 +2,24 @@ from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.types import Message, CallbackQuery
 import random
-from . import my_list
+from src import list
 
 router = Router()
 
 
 def get_rand_quote() -> str:
-    i = random.randrange((len(my_list.my_list)))
-    return my_list.my_list[i]
+    i = random.randrange((len(list.my_list)))
+    return list.my_list[i]
 
 
 @router.message(Command('quote'))
 async def command_about(message: Message) -> None:
-    i = random.randrange((len(my_list.my_list)))
+    i = random.randrange((len(list.my_list)))
     await message.answer(get_rand_quote())
 
 
 @router.callback_query(F.data == "quote")
 async def on_callback(query: CallbackQuery) -> None:
-    i = random.randrange((len(my_list.my_list)))
+    i = random.randrange((len(list.my_list)))
     await query.message.answer(get_rand_quote())
-    print(query)
+
